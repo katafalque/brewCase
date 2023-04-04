62ba8a3ee6bad8950a06079aa67a89376bb539cd
Alperen Karail <50772168+katafalque@users.noreply.github.com>
2023-04-05 02:01:49 +0300

    operations edited for new async scrapers.

--- operations.py
+++ operations.py
@@ -1,8 +1,10 @@
 from scraper.scraper import ScraperFactory
 from data.csv_reader import csv_io
 from data.book import Book
-from tqdm import tqdm
+import tqdm
 import logging
+import asyncio
+import random
 
 
 class Operator:
@@ -34,23 +36,14 @@ class Operator:
             book = csv_io.row_to_book(row)
             data_dict_title[book.title] = book
         return data_dict_title
-        
-    def __get_start_values_per_scraper(self, start : int = 0, end : int= 0) -> list:
-        logging.info("Preparing search intervals with respect to inputs..")
-        scraper_count = len(self.scrapers)
-        if start == 0 and end == 0:
-            start_values = [i * (int(self.book_count / scraper_count)) for i in range(scraper_count)]
-            return start_values
-        start_values = [i * (int((end - start) / scraper_count)) + start for i in range(scraper_count)]
-        return start_values
+
+    def __have_missing_values(self, book : Book) -> bool:
+        return (book.author != '' and book.isbn != '' and book.price != '' and book.page_size != '' and book.title != '')
         
     def __book_to_row(self, book : Book) -> list:
         if book:
             return [book.author, book.title, book.price, book.isbn, book.price]
         return False
-    
-    def __update_row(self, index : int, row : list):
-        self.data[index] = row
         
     def get_book_info_by_isbn(self, isbn : str) -> Book:
         book = self.data_dict_isbn.get(isbn)
@@ -60,48 +53,36 @@ class Operator:
         book = self.data_dict_title.get(title)
         return book
     
-    def process_missing_info(self, start : int = 0, end : int = 0) -> list:
-        logging.info("Processing missing info..")
-        start_values = self.__get_start_values_per_scraper(start, end)                
-        amazon = self.scrapers.get("Amazon")
-        google = self.scrapers.get("Google")
-        interval = int(self.book_count if start == 0 and end == 0 else (end - start) / 2)
-        amazon_list = self.data[start_values[0]:start_values[1]]
-        google_list = self.data[start_values[1]:start_values[1] + interval]
+    async def process_missing_info(self, start : int = 0, end : int = 0):
+        logging.info("Processing missing info..")  
+        
+        if start == 0 and end == 0:
+            start, end = 0, self.book_count
+        
+        tasks = []
 
-        amazon_results = []
-        google_results = []
-        progress_bar = tqdm(total = len(amazon_list) + len(amazon_list))
-        for index, row in enumerate(amazon_list):
+        for row in self.data[start : end - 1]:
+            random_scraper_index = random.randint(0, 1)
+            scraper = self.scrapers[random_scraper_index]
             book = csv_io.row_to_book(row)
-            if book and (book.author == '' or book.isbn == '' or book.title == '' or book.price != ''):
-                if book.author == '' or book.isbn == '' or book.title == '' or book.price != '':
-                    resp = amazon.get_book_amazon(book.isbn)
-                    amazon_results.append(self.__book_to_row(resp))
-                    self.__update_row(index + start_values[0], row)
-            progress_bar.update(1)
-                
-                
-        for index, row in enumerate(google_list):
-            print("in here")
-            book = csv_io.row_to_book(row)
-            if book and book.isbn != '' and (book.author == '' or book.isbn == '' or book.title == '' or book.price != ''):
-                resp = google.get_book_google(True, book.isbn)
-                if not resp:
-                    print(f"ISBN : {book.isbn} produced no results..")
-                else:
-                    google_results.append(self.__book_to_row(resp))
-                    self.__update_row(index + start_values[1], row)
-            elif book and book.title != '':
-                resp = google.get_book_google(False, book.title)
-                if not resp:
-                    print(f"Title : {book.title} produced no results..")
-                else:
-                    google_results.append(self.__book_to_row(resp))
-                    self.__update_row(index + start_values[1], row)  
+            if book.isbn != '':
+                task = asyncio.create_task(scraper.get_book(book.isbn))
+                tasks.append(task)
+            elif book.title != '':
+                task = asyncio.create_task(scraper.get_book(book.title))
+                tasks.append(task)
+            else:
+                tasks.append(asyncio.sleep(0))
+
+        books = [await f for f in tqdm.tqdm(asyncio.as_completed(tasks), total=len(tasks))]
+        
+        for index, book in enumerate(books):
+            row = self.__book_to_row(book)
+            self.data[index + start] = row
             
-            progress_bar.update(1)
-    
+        self.update_csv()
+            
+            
     def update_csv(self) -> None:
         csv_io.write_csv(self.file_path, self.data)
         
