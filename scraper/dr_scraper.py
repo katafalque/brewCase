ffa00f491ed363c221d38dcd22a05d4579cb8d27
Alperen Karail <50772168+katafalque@users.noreply.github.com>
2023-04-04 21:29:25 +0300

    Fixed a minor bug preventing printing logs to file.

--- scraper/dr_scraper.py
+++ scraper/dr_scraper.py
@@ -1,6 +1,7 @@
 import aiohttp
 import logging
 import traceback
+import asyncio
 from data.book import Book
 from bs4 import BeautifulSoup
 
@@ -16,7 +17,7 @@ class DR:
 
     async def __get_book_href_dr(self, query : str) -> str:
         try:
-            logging.info(f"Trying execute : {self.__get_book_href_dr.func_name} for query : {query}")
+            logging.info(f"Trying execute : {self.__get_book_href_dr.__name__} for query : {query}")
             
             params = self.__get_params_dr(query)
             async with aiohttp.ClientSession() as session:
@@ -26,33 +27,33 @@ class DR:
             soup = BeautifulSoup(response,"html.parser")
             href = soup.find("a",{"class":"js-search-prd-item"}).get("href")
             
-            logging.info(f"Execution of : {self.__get_book_href_dr.func_name} for query : {query} was successfull")
+            logging.info(f"Execution of : {self.__get_book_href_dr.__name__} for query : {query} was successfull")
             
             return href
         except Exception:
             
-            logging.info(f"Something went wrong during execution : {self.__get_book_href_dr.func_name} for query : {query}")
+            logging.info(f"Something went wrong during execution : {self.__get_book_href_dr.__name__} for query : {query}")
             logging.info(f"Exception traceback : {traceback.format_exc()}")
             
             return False
     
     async def __get_book_page_dr(self, query : str) -> str:
         try:
-            logging.info(f"Trying execute : {self.__get_book_page_dr.func_name} for query : {query}")
+            logging.info(f"Trying execute : {self.__get_book_page_dr.__name__} for query : {query}")
             
             href = await self.__get_book_href_dr(query)
-            if not href:
+            if href:
                 async with aiohttp.ClientSession() as session:
                     async with session.get(f"https://www.dr.com.tr/{href}") as response:
                         response = await response.content.read()
                 
-                logging.info(f"Execution of : {self.__get_book_page_dr.func_name} for query : {query} was successfull")
+                logging.info(f"Execution of : {self.__get_book_page_dr.__name__} for query : {query} was successfull")
                 
                 return response
             
         except Exception:
                 
-                logging.info(f"Something went wrong during execution : {self.__get_book_page_dr.func_name} for query : {query}")
+                logging.info(f"Something went wrong during execution : {self.__get_book_page_dr.__name__} for query : {query}")
                 logging.info(f"Exception traceback : {traceback.format_exc()}")
                 
                 return False
@@ -61,10 +62,10 @@ class DR:
     async def __book_page_parser_dr(self, query : str) -> dict:
         try:
             
-            logging.info(f"Trying execute : {self.__book_page_parser_dr.func_name} for query : {query}")
+            logging.info(f"Trying execute : {self.__book_page_parser_dr.__name__} for query : {query}")
             
             page = await self.__get_book_page_dr(query)
-            if not page:
+            if page:
                 parser = BeautifulSoup(page,"html.parser")
                 datas = parser.find("div", {"class":"product-property"}).find("ul")
                 
@@ -80,34 +81,30 @@ class DR:
                 
                 book_data["price"] = price
                 
-                logging.info(f"Execution of : {self.__book_page_parser_dr.func_name} for query : {query} was successfull")
+                logging.info(f"Execution of : {self.__book_page_parser_dr.__name__} for query : {query} was successfull")
                 
                 return book_data
             
         except Exception:
                 
-            logging.info(f"Something went wrong during execution : {self.__book_page_parser_dr.func_name} for query : {query}")
+            logging.info(f"Something went wrong during execution : {self.__book_page_parser_dr.__name__} for query : {query}")
             logging.info(f"Exception traceback : {traceback.format_exc()}")
             
             return False
     
     async def get_book(self, query : str) -> Book:
         try:
-            logging.info(f"Trying execute : {self.get_book.func_name} D&R for query : {query}")
+            logging.info(f"Trying execute : {self.get_book.__name__} D&R for query : {query}")
             
             book_data = await self.__book_page_parser_dr(query)
-            if not book_data:
+            if book_data:
                 
-                logging.info(f"Execution of : {self.__book_page_parser_dr.func_name} D&R for query : {query} was successfull")
+                logging.info(f"Execution of : {self.__book_page_parser_dr.__name__} D&R for query : {query} was successfull")
                 
                 return Book(book_data.get("isbn"), book_data.get("title"), book_data.get("page_size"), book_data.get("author"), book_data.get("price"))
             
         except Exception:
-            logging.info(f"Something went wrong during execution : {self.get_book.func_name} D&R for query : {query}")
+            logging.info(f"Something went wrong during execution : {self.get_book.__name__} D&R for query : {query}")
             logging.info(f"Exception traceback : {traceback.format_exc()}")
             
             return False
-    
-
-    
-    
